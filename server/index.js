ad38c0e374e4864ad1d696cb42e687298091caf0
Syed Malik Faraz <smalikfaraz@gmail.com>
2026-06-02 20:58:51 +0530

    Adding /basic route

--- server/index.js
+++ server/index.js
@@ -36,6 +36,10 @@ app.use(cors({
   credentials: true,
 }));
 
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
